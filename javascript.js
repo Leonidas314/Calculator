fc5e3908cd3cb12c6897111e52fe58dbd449da90
Leon <leondellafiore2016@gmail.com>
2024-03-06 21:25:02 -0300

    Correccion en botones js

--- javascript.js
+++ javascript.js
@@ -1,21 +1,27 @@
 console.log("Console online");
-const calculador= document.getElementById('calcBody');
+const calculador= document.getElementById('calcBody');//Capturar div id:"calcBody"
 console.log(calculador);
-const tds =document.querySelectorAll('td');
+const tds =document.querySelectorAll('td');//Captura tds de la tabla
 console.log(tds);
-const pantalla =document.getElementById('screenCalc');
-var texto="";
-tds.forEach(function(elemento){
-    elemento.addEventListener('click',function(elemento){
-        this.style.transform= 'scale(0.9)';
-        setTimeout(() => {
+const pantalla =document.getElementById('screenCalc');//Captura div id:"screenCalc"
+
+
+var ceroDefault = true;
+
+tds.forEach(function(elemento){//Para cada tds
+    elemento.addEventListener('click',function(elemento){//Al capturar evento click
+        this.style.transform= 'scale(0.9)';//Cambio a escala 0.9
+        setTimeout(() => {//Vuelvo a escala 1 a los 150ms
             this.style.transform = 'scale(1)';
         }, 150);
-        const contenidoBoton=this.innerText;
-        // texto=texto+contenidoBoton;
+        if (ceroDefault==true){
+            pantalla.innerText="";
+            ceroDefault=false;
+        }
+        const contenidoBoton=this.innerText;//Capturo el contenido alfanumerico de td (boton)
         console.log(contenidoBoton);
-        console.log(texto);
-        const textoNodo =document.createTextNode(contenidoBoton);
-        pantalla.appendChild(textoNodo)
+        const textoNodo =document.createTextNode(contenidoBoton);//Creo el nodo con el contenido capturado
+        pantalla.appendChild(textoNodo)//creo un nodo nuevo "hijo" de pantalla
     })
 })
+
