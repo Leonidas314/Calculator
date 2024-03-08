1fe2c26b3a707cc0ef5d0beadf5331880b5be58f
Leon <leondellafiore2016@gmail.com>
2024-03-08 19:24:46 -0300

    Error si el primer elemetno es un simbolo matematico

--- javascript.js
+++ javascript.js
@@ -16,8 +16,8 @@ var screenString = "";
 tds.forEach(function(elemento){//Para cada tds
     elemento.addEventListener('click',function(elemento){//Al capturar evento click
         this.style.transform= 'scale(0.9)';//Cambio a escala 0.9
-        setTimeout(() => {//Vuelvo a escala 1 a los 150ms
-            this.style.transform = 'scale(1)';
+        setTimeout(() => {//Vuelvo a escala a los 150ms
+            this.style.transform = 'scale)';
         }, 150);
         if (ceroDefault==true){
             pantalla.innerText="";
@@ -29,6 +29,8 @@ tds.forEach(function(elemento){//Para cada tds
 
         const textoNodo =document.createTextNode(contenidoBoton);//Creo el nodo con el contenido capturado
         pantalla.appendChild(textoNodo)//creo un nodo nuevo "hijo" de pantalla
+        console.log(screenString.length)
+
     })
 })
 
@@ -37,6 +39,7 @@ const clearBoton = document.getElementById('clearBotton');
 clearBoton.addEventListener('click',function(){
     pantalla.innerText="";
     pantalla.innerText="0";
+    screenString="";
     ceroDefault=true;
 })
 
@@ -49,10 +52,14 @@ clearBoton.addEventListener('click',function(){
 //I need to check that if at both sides of each mathematical symbol there´s only numbesr, in order to have an expression mathematically logical, this is, for example: 2+2 is fine, +3*/ isn´t.
 
 const equalButom= document.getElementById('equalBotton');
-
+const symbols = document.getElementsByClassName('symbol');
 equalButom.addEventListener('click',function(){
-for (let i = 0; i < screenString.length; i++) {
-    console.log(str[i]);
-}
-
+    let screenL=screenString.length
+    for (let i = 0; i < screenString.length; i++) {
+       if(screenString[0]=="/"||screenString[0]=="*"||screenString[0]=="-"||screenString[0]=="+"/*||screenString[screenL]=="/"||screenString[screenL-1]=="*"||screenString[screenL]=="-"||screenString[screenL]=="+"*/){
+        pantalla.innerText="SYNTHAX ERROR";
+        screenString="";
+        console.log(screenL);
+       }
+    }
 })
