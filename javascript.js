884090bdafdd8e9704ae87a151ba4db2fb0f2946
Leon <leondellafiore2016@gmail.com>
2024-03-10 20:54:47 -0300

    equal key recognize when there's two consecutive mathematical simbols, and print error on screen

--- javascript.js
+++ javascript.js
@@ -89,6 +89,16 @@ equalButom.addEventListener('click',function(){
             disableScreen=true;
             console.log(screenString.length)
             console.log(screenString[screenString.length]);
+       }else
+       if(screenString.length>2){
+            for(let i=1;i<screenString.length-1;i++){
+                if(screenString[i]=="/"||screenString[i]=="*"||screenString[i]=="-"||screenString[i]=="+"){
+                    if(screenString[i-1]=="/"||screenString[i-1]=="*"||screenString[i-1]=="-"||screenString[i-1]=="+"){
+                        pantalla.innerText="SYNTHAX ERROR";
+                        disableScreen=true;
+                    }
+                }
+            }
        }
        console.log(screenString.length)
 
