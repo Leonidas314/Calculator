18ce040cfd411b593cd81c8fd24b435ef3624277
Leon <leondellafiore2016@gmail.com>
2024-04-13 19:26:38 -0300

    Listas con valores numericos y operadores ya filtrados con explesiones regulares

--- funcional.js
+++ funcional.js
@@ -1,11 +1,20 @@
 
 console.log("Funcion  filtar expresiones numericas")
 
-//Function that convert the first numerical value into a list with the digits 
+//Function that reccibe a string and return a list of the numeric values 
 
-function listFirtsNum (string){
-    var list = string.match(/\d+/g)
-    return list;
+
+let string1="44*3*456/55/88"
+
+function listNumValues (string){
+    return string.match(/\d+/g);;
 }
 
-console.log(listFirtsNum("44+55Hola88"));
+function listOperator (string){
+    return string.match(/[^\d]/g);
+}
+
+
+console.log(listNumValues(string1),"\n", listOperator(string1));
+
+//Next to do: numeric values on list from string to Float and "merge" the list with operator on list operator
